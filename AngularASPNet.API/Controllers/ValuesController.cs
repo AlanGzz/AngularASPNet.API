4f44762bcc04ac939d151bfd7c0ffb0558f93959
Alan <alangeragzz@gmail.com>
2018-12-17 11:33:36 -0600

    Creacion de componentes Angular 6

--- AngularASPNet.API/Controllers/ValuesController.cs
+++ AngularASPNet.API/Controllers/ValuesController.cs
@@ -22,6 +22,7 @@ namespace AngularASPNet.API.Controllers
         }
 
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
